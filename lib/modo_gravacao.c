76a3bc6580e5a78b1ac3f15ac9a67d05cb25ac46
01Keven <kevenchristiancode@gmail.com>
2025-01-28 15:24:36 -0300

    feat: adicionando modo de gravacao

--- lib/modo_gravacao.c
+++ lib/modo_gravacao.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
-#include "lib/keypad.h" // Inclua se estiver usando a biblioteca de teclado
+#include "lib/keypad.h"
 
 /**
- * Função que verifica se a tecla '*' foi pressionada e, caso positivo,
- * reinicia o Raspberry Pi Pico no modo de gravação.
- * 
  * @param key_code Código da tecla pressionada.
  */
 void verificar_modo_gravacao(char key_code) {
